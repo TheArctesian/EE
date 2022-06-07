9846a799632e4b186a9c871e464c6a9f1c36b2ad
TheArctesian <stephen.d.okita@gmail.com>
2022-06-07 11:57:20 +0800

    price

--- Fetchers/Price/rapid.py
+++ Fetchers/Price/rapid.py
@@ -1,4 +1,7 @@
 import requests
+# import convert
+import json
+import time
 
 url = "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history"
 
@@ -11,4 +14,13 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+
+# loop through response and change the vaule of timestamp to a human redable format using time 
+data = response.json()
+for i in range(len(data['data']['history'])):
+	data['data']['history'][i]['timestamp'] = time.strftime("%d/%m/%Y", time.localtime(data['data']['history'][i]['timestamp']))
+
+with open('out.json', 'w') as f:
+	f.write(json.dumps(data))
+
+
