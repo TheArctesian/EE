e360e0d90cc3c384c0a4d448f40243760fe972fe
TheArctesian <stephen.d.okita@gmail.com>
2022-06-09 08:02:49 +0800

    twitter is done

--- Sent/sentiment.py
+++ Sent/sentiment.py
@@ -4,3 +4,6 @@ positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+
+
+# https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
