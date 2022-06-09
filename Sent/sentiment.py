582c97a040ff5bcde856e87ab198846fdd2fc921
TheArctesian <stephen.d.okita@gmail.com>
2022-06-09 09:38:05 +0800

    sent

--- Sent/sentiment.py
+++ Sent/sentiment.py
@@ -1,9 +1,6 @@
-from nltk.corpus import twitter_samples
-
-positive_tweets = twitter_samples.strings('positive_tweets.json')
-negative_tweets = twitter_samples.strings('negative_tweets.json')
-text = twitter_samples.strings('tweets.20150430-223406.json')
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-
-
 # https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
+import nltk
+nltk.download()
+sen = "I hate this world and I want to die"
+tokens = nltk.word_tokenize(sen)
+print(tokens)
