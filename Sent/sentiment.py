7b9f73b12e60424e09581a78147c43fc5f9318eb
theArctesian <stephen.d.okita@gmail.com>
2022-06-09 11:46:34 +0800

    pee pee

--- Sent/sentiment.py
+++ Sent/sentiment.py
@@ -1,6 +1,13 @@
 # https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
-import nltk
-nltk.download()
+from nltk import SentimentIntensityAnalyzer
+# nltk.download()
 sen = "I hate this world and I want to die"
 tokens = nltk.word_tokenize(sen)
 print(tokens)
+tagged = nltk.pos_tag(tokens)
+print(tagged)
+
+t = treebank.parsed_sents('thing.mrg')[0]
+t.draw()
+
+
