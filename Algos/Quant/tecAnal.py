398533ab1bc25899813b6c9cc13fe1d3eff37176
TheArctesian <stephen.d.okita@gmail.com>
2022-06-21 17:13:05 -0700

    logger and stuff

---
+++ Algos/Quant/tecAnal.py
@@ -0,0 +1,36 @@
+import os
+import logging
+import pandas as pd
+
+# LOGGING 
+class handler(logging.StreamHandler):
+    colors = {
+        logging.DEBUG: '\033[37m',
+        logging.INFO: '\033[36m',
+        logging.WARNING: '\033[33m',
+        logging.ERROR: '\033[31m',
+        logging.CRITICAL: '\033[101m',
+    }
+    reset = '\033[0m'
+    fmtr = logging.Formatter('%(levelname)s %(message)s')
+
+    def format(self, record):
+        color = self.colors[record.levelno]
+        log = self.fmtr.format(record)
+        reset = self.reset
+        return color + log + reset
+
+
+logging.basicConfig(level=logging.DEBUG, handlers=[handler()])
+logger = logging.getLogger('bobcat')
+logger.setLevel('DEBUG')
+
+
+
+def readData():
+    price = pd.read_csv('price.csv')
+    logging.info(price.head())
+
+if __name__ == "__main__":
+    readData()
+    
